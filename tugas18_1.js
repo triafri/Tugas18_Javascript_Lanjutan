b39b54db9c5dc50a98772141e2bdc6b053b88d39
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-21 11:30:57 +0700

    Add files via upload

---
+++ tugas18_1.js
@@ -0,0 +1,5 @@
+var kalimat ="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout"
+
+ambil = kalimat.split(" ")
+
+console.log(ambil[3], ambil[4])
